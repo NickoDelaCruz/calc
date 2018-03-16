dcfa093e0f82e5197c28e3c22e9b74ef956cf0fc
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-16 15:29:26 -0700

    first spec pass

---
+++ spec/calculator-spec.js
@@ -0,0 +1,5 @@
+import { Calculator } from './../src/calculator-logic.js';
+
+describe ("", function () {
+  it("Converts dateTime into seconds", function() {
+  })
--- src/calculator-logic.js
+++ src/calculator-logic.js
@@ -1,11 +1,14 @@
 export class Calculator {
   constructor() {
-
+    this.birthDate;
+    this.todayDate;
+    this.DateInSeconds;
+    this.todaysDateInSeconds;
+    this.usersAge;
   }
 
-
-
-
-
-
+  ToSeconds() {
+    this.userDateInSeconds = ((this.birthDate.getTime())/1000);
+    this.todaysDateInSeconds = ((this.todayDate.getTime())/1000);
+  }
 }
