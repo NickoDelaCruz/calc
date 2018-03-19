ac71a596f75effee6e3872415576228ebe4fe96d
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-18 18:47:30 -0700

    cleaned up calculator-spec

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -14,30 +14,30 @@ describe('Calculator', function() {
   });
 
   it('Takes users age and converts it into seconds', function() {
-    let Age = new Calculator(5, "5 years");
-    expect(Age.age2Seconds()).toBeCloseTo(157680000)
+    let Age = new Calculator(1, "1 year");
+    expect(Age.age2Seconds()).toBeCloseTo(31536000)
   });
 
-  it('Takes userBday and converts it into Mercury Age', function() {
+  it('Takes age and converts it into Mercury Age', function() {
     let Age = new Calculator(21, "11-21-1996");
     expect(Age.age2Mercury()).toBeCloseTo(87);
   });
 
-  it('Takes userBday and converts it into Venus Age', function() {
+  it('Takes age and converts it into Venus Age', function() {
     let Age = new Calculator(21, "11-21-1996");
     expect(Age.age2Venus()).toBeCloseTo(33);
   });
 
-  it('Takes userBday and converts it into Mars Age', function() {
+  it('Takes age and converts it into Mars Age', function() {
     let Age = new Calculator(21, "11-21-1996");
     expect(Age.age2Mars()).toBeCloseTo(11);
   });
 
-  it('Takes userBday and converts it into Jupiter Age', function() {
+  it('Takes age and converts it into Jupiter Age', function() {
     let Age = new Calculator(55, "11-21-1963");
     expect(Age.age2Jupiter()).toBeCloseTo(4);
   });
-    it('Takes userBday and adds 150 then divide by 2 for a average life expectancy on earth', function() {
+    it('Takes users age and calculates a life expectancy on earth', function() {
     var avg = new Calculator(21);
     expect(avg.AvgLifeEarth()).toBeLessThanOrEqual(100)
   });
